9b186ba3a6c792bad70f0bb2e0c1143513560551
edtallison <edtallison@gmail.com>
2022-02-07 16:36:10 +1300

    Add if name main check

--- src/main.py
+++ src/main.py
@@ -3,38 +3,43 @@ from classes.paddle import Paddle
 from classes.ball import Ball
 pygame.init()
 
-size = width, height = 1200, 800
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Pong!")
+def main():
 
-black = (0,0,0)
-white = (255,255,255)
+    size = width, height = 1200, 800
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Pong!")
 
-pLeft = Paddle(100, height, white)
-pRight = Paddle(width - 150, height, white)
-ball = Ball(width, height, white)
+    black = (0,0,0)
+    white = (255,255,255)
 
-# Add clock/time to smooth out framerate
-clock = pygame.time.Clock()
+    pLeft = Paddle(100, height, white)
+    pRight = Paddle(width - 150, height, white)
+    ball = Ball(width, height, white)
 
-# Game loop
-while 1:
-    # Lets the user close the game
-    for event in pygame.event.get():
-       if event.type == pygame.QUIT:
-            sys.exit()
-    # Get key presses from user
-    keys = pygame.key.get_pressed()
+    # Add clock/time to smooth out framerate
+    clock = pygame.time.Clock()
 
-    pLeft.move(keys[pygame.K_w], keys[pygame.K_s])
-    pRight.move(keys[pygame.K_UP], keys[pygame.K_DOWN])
-    ball.move()
+    # Game loop
+    while 1:
+        # Lets the user close the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    sys.exit()
+        # Get key presses from user
+        keys = pygame.key.get_pressed()
 
-    # Update screen with positions of paddles
-    screen.fill(black)
-    pLeft.update_pos(screen)
-    pRight.update_pos(screen)
-    ball.update_pos(screen)
-    pygame.display.flip()
+        pLeft.move(keys[pygame.K_w], keys[pygame.K_s])
+        pRight.move(keys[pygame.K_UP], keys[pygame.K_DOWN])
+        ball.move()
 
-    clock.tick(30)
+        # Update screen with positions of paddles
+        screen.fill(black)
+        pLeft.update_pos(screen)
+        pRight.update_pos(screen)
+        ball.update_pos(screen)
+        pygame.display.flip()
+
+        clock.tick(30)
+
+if __name__ == "__main__":
+    main()
