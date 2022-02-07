bcfd6bee2da07955ae19ebdb692bb56a42d8910c
edtallison <edtallison@gmail.com>
2022-02-07 14:59:42 +1300

    Add player movement using the power of OOP

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from re import L
 import sys, pygame
 pygame.init()
 
@@ -12,9 +11,14 @@ white = (255,255,255)
 class player:
     def __init__(self, x_pos):
         self.x_pos = x_pos
-        self.y_pos = 400
+        self.y_pos = 300
+        # Create the rectangle to be drawn on the screen
         self.surf = pygame.Surface((50, 200))
         self.surf.fill(white)
+
+    def move(self, up, down):
+        self.y_pos -= up * 10
+        self.y_pos += down * 10
 
     def update_pos(self):
         screen.blit(self.surf, (self.x_pos,self.y_pos))
@@ -22,20 +26,25 @@ class player:
 pLeft = player(100)
 pRight = player(1050)
 
+# Add clock/time to smooth out framerate
+clock = pygame.time.Clock()
+
 # Game loop
 while 1:
-    
     # Lets the user close the game
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
             sys.exit()
-
+    # Get key presses from user
     keys = pygame.key.get_pressed()
+
+    pLeft.move(keys[pygame.K_w], keys[pygame.K_s])
+    pRight.move(keys[pygame.K_UP], keys[pygame.K_DOWN])
 
     # Update screen with positions of players
     screen.fill(black)
-
     pLeft.update_pos()
     pRight.update_pos()
-
     pygame.display.flip()
+
+    clock.tick(30)
