f0d52903a8de2643db66c53d0e98859e86f899f0
edtallison <edtallison@gmail.com>
2022-02-07 16:53:46 +1300

    Implement ball bouncing off walls

--- src/classes/ball.py
+++ src/classes/ball.py
@@ -2,15 +2,27 @@ import pygame
 from classes.object import Object
 
 class Ball(Object):
-    def __init__(self, width, height, colour):
+    def __init__(self, screen_width, screen_height, colour):
         self.dim = 30
         self.size = (self.dim, self.dim)
-        self.x_pos = (width/2) - (self.dim/2)
-        self.y_pos = (height/2) - (self.dim/2)
+
+        self.s_w = screen_width
+        self.s_h = screen_height 
+        self.x_pos = (self.s_w/2) - (self.dim/2)
+        self.y_pos = (self.s_h/2) - (self.dim/2)
 
         self.surf = pygame.Surface(self.size)
         self.surf.fill(colour)
+
+        self.vertical = 1
+        self.horizontal = 1
     
     def move(self):
-        self.x_pos += 10
-        self.y_pos -= 10
+
+        if (self.y_pos < 0) or (self.y_pos > (self.s_h - self.dim)):
+            self.vertical *= -1
+        if (self.x_pos > (self.s_w - self.dim)) or (self.x_pos < 0):
+            self.horizontal *= -1
+
+        self.x_pos += self.horizontal * 10
+        self.y_pos -= self.vertical * 10
