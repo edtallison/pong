d173d7789e1f2e56b3658041906014453b0df012
edtallison <edtallison@gmail.com>
2022-02-07 15:23:38 +1300

    Add ball class

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys, pygame
 pygame.init()
 
-size = width, height = 1500, 800
+size = width, height = 1200, 800
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong!")
 
 black = (0,0,0)
 white = (255,255,255)
 
-class player:
+class paddle:
     def __init__(self, x_pos):
         self.height = 200
         self.x_pos = x_pos
@@ -28,8 +28,22 @@ class player:
     def update_pos(self):
         screen.blit(self.surf, (self.x_pos,self.y_pos))
 
-pLeft = player(100)
-pRight = player(width - 150)
+class ball:
+    def __init__(self):
+        self.dim = 30
+        self.size = (self.dim, self.dim)
+        self.x_pos = (width/2) - (self.dim/2)
+        self.y_pos = (height/2) - (self.dim/2)
+
+        self.surf = pygame.Surface(self.size)
+        self.surf.fill(white)
+
+    def update_pos(self):
+        screen.blit(self.surf, (self.x_pos,self.y_pos))
+
+pLeft = paddle(100)
+pRight = paddle(width - 150)
+ball = ball()
 
 # Add clock/time to smooth out framerate
 clock = pygame.time.Clock()
@@ -46,10 +60,11 @@ while 1:
     pLeft.move(keys[pygame.K_w], keys[pygame.K_s])
     pRight.move(keys[pygame.K_UP], keys[pygame.K_DOWN])
 
-    # Update screen with positions of players
+    # Update screen with positions of paddles
     screen.fill(black)
     pLeft.update_pos()
     pRight.update_pos()
+    ball.update_pos()
     pygame.display.flip()
 
     clock.tick(30)
